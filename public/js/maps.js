44e5e8283e1fbea18773a2ee2c733da2af09b651
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-01-29 10:52:42 +0100

    Fixed indentation & added clearpins function

--- public/js/maps.js
+++ public/js/maps.js
@@ -1,122 +1,129 @@
 function initialize() {
 
-    var mapOptions = {
-        zoom: 3,
-        center: { lat: -34.397, lng: 150.644},
-        disableDefaultUI: true,
-        zoomControl: true
-    }
+  var mapOptions = {
+      zoom: 3,
+      center: { lat: -34.397, lng: 150.644},
+      disableDefaultUI: true,
+      zoomControl: true
+  }
       
-    var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-    map.set('styles', [
-        {
-            "featureType": "administrative",
-            "elementType": "labels.text.fill",
-            "stylers": [
-                {
-                    "color": "#444444"
-                }
-            ]
-        },
-        {
-            "featureType": "landscape",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "color": "#f2f2f2"
-                }
-            ]
-        },
-        {
-            "featureType": "poi",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "road",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "saturation": -100
-                },
-                {
-                    "lightness": 45
-                }
-            ]
-        },
-        {
-            "featureType": "road.highway",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "visibility": "simplified"
-                }
-            ]
-        },
-        {
-            "featureType": "road.arterial",
-            "elementType": "labels.icon",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "transit",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "visibility": "off"
-                }
-            ]
-        },
-        {
-            "featureType": "water",
-            "elementType": "all",
-            "stylers": [
-                {
-                    "color": "#46bcec"
-                },
-                {
-                    "visibility": "on"
-                }
-            ]
+  map.set('styles', [
+      {
+          "featureType": "administrative",
+          "elementType": "labels.text.fill",
+          "stylers": [
+              {
+                  "color": "#444444"
+              }
+          ]
+      },
+      {
+          "featureType": "landscape",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "color": "#f2f2f2"
+              }
+          ]
+      },
+      {
+          "featureType": "poi",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "road",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "saturation": -100
+              },
+              {
+                  "lightness": 45
+              }
+          ]
+      },
+      {
+          "featureType": "road.highway",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "simplified"
+              }
+          ]
+      },
+      {
+          "featureType": "road.arterial",
+          "elementType": "labels.icon",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "transit",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "visibility": "off"
+              }
+          ]
+      },
+      {
+          "featureType": "water",
+          "elementType": "all",
+          "stylers": [
+              {
+                  "color": "#46bcec"
+              },
+              {
+                  "visibility": "on"
+              }
+          ]
+      }
+  ]);
+
+  var pins = []
+
+  $('#search-tour').click(function(e) {
+    console.log('clicked');
+    $.ajax({
+        url: '/tourdata',
+        data: 'id=' + $('#id').val(),
+        context: document.body,
+        success: function(res) {
+          clearPins(pins);
+          var data = $.parseJSON(res);
+          $.each(data, function(i, item) {
+            var pos = new google.maps.LatLng(item.latitude, item.longitude);
+            var marker = new google.maps.Marker({
+                position: pos,
+                map: map,
+                title: 'test'
+            });
+
+            pins.push(marker);
+          });
         }
-    ]);
+      })
 
-    
-    $('#search-tour').click(function(e) {
-      console.log('clicked');
+      e.preventDefault();
+  });
+}
 
-        $.ajax({
-            url: '/tourdata',
-            data: 'id=' + $('#id').val(),
-            context: document.body,
-            success: function(res) {
-              var data = $.parseJSON(res);
-              $.each(data, function(i, item) {
-                var pos = new google.maps.LatLng(item.x, item.y);
-                var marker = new google.maps.Marker({
-                    position: pos,
-                    map: map,
-                    title: 'test'
-                });
+function clearPins(pins) {
+  for (i in pins) {
+      pins[i].setMap(null);
+  }
 
-                console.log(item);
-              });
-
-              console.log(res);
-            }
-          })
-
-        e.preventDefault();
-    });
+  pins.length = 0;
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);

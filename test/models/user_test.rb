9b28390fc082082c4421d0905ae48e32de4b6dc8
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-02-02 11:06:35 +0100

    Name test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,13 +3,13 @@ require_relative 'models_helper'
 describe User do
 
   before do
-    @user = User.new('Reginald', 'Fartswaith')
+    @user = User.first(first_name: 'Refugio')
   end
 
   describe 'name convenience method' do
 
     it 'should return first and last names' do
-      @user.name.should match 'Reginald Fartswaith'
+      @user.name.should match 'Refugio Hertzog'
     end
 
   end

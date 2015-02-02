2bb5ee7afc7b6320b2db0378cdec0c9d8bc7488f
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-02-02 11:06:48 +0100

    Disabled rspec colors

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,10 +9,10 @@ require 'rspec'
 
 RSpec.configure do |config|
   # Use color in STDOUT
-  config.color_enabled = true
+  #config.color_enabled = true
 
   # Use color not only in STDOUT but also in pagers and files
-  config.tty = true
+  #config.tty = true
 
   config.formatter = :documentation#, :progress, :html, :textmate
 end

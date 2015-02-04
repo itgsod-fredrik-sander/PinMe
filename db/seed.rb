f62947be57d40cc636fea6565a51195bb803388e
itgsod-oscar-johansson <oscar.johansson3@Administrators-MacBook-Pro-60.local>
2015-02-04 14:01:02 +0100

    Renameing

--- db/seed.rb
+++ db/seed.rb
@@ -10,15 +10,15 @@ class Seed
 
   def self.pins
     pins = [
-        { sequencepos: 0, description: "Pin number one is a fine pun", title: 'Pin one', img_url: "smexy.png", longitude: 12.3, latitude: 33.3, tour_id: 1 },
-        { sequencepos: 1, description: "Pin number two is a fine pun",  title: 'Pin two', img_url: "lexicon.png", longitude: 24.3, latitude: 45.3, tour_id: 1 },
-        { sequencepos: 2, description: "Pin number three is a fine pun", title: 'Pin three', img_url: "lyxfisk.png", longitude: 36.3, latitude: 57.3, tour_id: 1 },
-        { sequencepos: 3, description: "Pin number four is a fine pun", title: 'Pin four', img_url: "tacobiff.png", longitude: 58.3, latitude: 69.3, tour_id: 1 },
+        { sequence_pos: 0, description: "Pin number one is a fine pun", title: 'Pin one', img_url: "smexy.png", longitude: 12.3, latitude: 33.3, tour_id: 1 },
+        { sequence_pos: 1, description: "Pin number two is a fine pun",  title: 'Pin two', img_url: "lexicon.png", longitude: 24.3, latitude: 45.3, tour_id: 1 },
+        { sequence_pos: 2, description: "Pin number three is a fine pun", title: 'Pin three', img_url: "lyxfisk.png", longitude: 36.3, latitude: 57.3, tour_id: 1 },
+        { sequence_pos: 3, description: "Pin number four is a fine pun", title: 'Pin four', img_url: "tacobiff.png", longitude: 58.3, latitude: 69.3, tour_id: 1 },
 
-        { sequencepos: 0, description: "Pin number one is a fine pun", title: 'Pin two one', img_url: "smexy.png", longitude: 30.3, latitude: 45.3, tour_id: 2 },
-        { sequencepos: 1, description: "Pin number two is a fine pun", title: 'Pin two two', img_url: "lexicon.png", longitude: 30.3, latitude: 15.3, tour_id: 2 },
-        { sequencepos: 2, description: "Pin number three is a fine pun", title: 'Pin two three', img_url: "lyxfisk.png", longitude: 30.3, latitude: 20.3, tour_id: 2 },
-        { sequencepos: 3, description: "Pin number four is a fine pun", title: 'Pin two four', img_url: "tacobiff.png", longitude: 30.3, latitude: 10.3, tour_id: 2 }
+        { sequence_pos: 0, description: "Pin number one is a fine pun", title: 'Pin two one', img_url: "smexy.png", longitude: 30.3, latitude: 45.3, tour_id: 2 },
+        { sequence_pos: 1, description: "Pin number two is a fine pun", title: 'Pin two two', img_url: "lexicon.png", longitude: 30.3, latitude: 15.3, tour_id: 2 },
+        { sequence_pos: 2, description: "Pin number three is a fine pun", title: 'Pin two three', img_url: "lyxfisk.png", longitude: 30.3, latitude: 20.3, tour_id: 2 },
+        { sequence_pos: 3, description: "Pin number four is a fine pun", title: 'Pin two four', img_url: "tacobiff.png", longitude: 30.3, latitude: 10.3, tour_id: 2 }
     ]
 
     pins.each {|pin| Pin.create(pin)}
@@ -26,10 +26,10 @@ class Seed
 
   def self.settings
     settings = [
-        {zoomlevel: 6, language: "English", user_id: 1},
-        {zoomlevel: 3, language: "English", user_id: 2},
-        {zoomlevel: 5, language: "English", user_id: 3},
-        {zoomlevel: 2, language: "English", user_id: 4}
+        {zoom_level: 6, language: "English", user_id: 1},
+        {zoom_level: 3, language: "English", user_id: 2},
+        {zoom_level: 5, language: "English", user_id: 3},
+        {zoom_level: 2, language: "English", user_id: 4}
     ]
 
     settings.each {|setting| Setting.create(setting)}

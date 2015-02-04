416b7b6702ad8a01c7c63a16aa25099611137294
itgsod-oscar-johansson <oscar.johansson3@Administrators-MacBook-Pro-60.local>
2015-02-04 13:50:56 +0100

    renamed zoom level to zoom_level

--- models/setting.rb
+++ models/setting.rb
@@ -2,7 +2,7 @@ class Setting
   include DataMapper::Resource
 
   property :id, Serial #TODO: Should not have ID, fix later alligator
-  property :zoomlevel, Integer
+  property :zoom_level, Integer
   property :language, String
 
   belongs_to :user

a0b6020bd3ac8a36ba65060135c8117540f5be8d
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-02-11 12:35:25 +0100

    Changed datatype of two properties

--- models/line.rb
+++ models/line.rb
@@ -2,8 +2,8 @@ class Line
   include DataMapper::Resource
 
   property :id, Serial
-  property :latitude, Integer
-  property :longitude, Integer
+  property :latitude, Float
+  property :longitude, Float
   property :sequence_pos, Integer
   property :free_move, Boolean
 

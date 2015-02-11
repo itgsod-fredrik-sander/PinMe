069cdcaf828331f5c9b2c554ae89054d271d28f8
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-02-11 12:35:38 +0100

    Returns the lines in the right sequencepos

--- routes/get_lines.rb
+++ routes/get_lines.rb
@@ -4,7 +4,7 @@ module Get
     def self.registered(app) 
       app.get '/lines' do 
         if session[:tour_id] 
-          Tour.get(session[:tour_id]).lines.to_json
+          Tour.get(session[:tour_id]).lines.sort_by {|l| l.sequence_pos}.to_json
         end
       end
     end

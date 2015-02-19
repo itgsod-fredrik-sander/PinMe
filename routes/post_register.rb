5a5846b29e7a016748ede5dc43470f89d4d091d2
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-02-19 10:49:11 +0100

    Registers user Fixes #43, fixes #43

--- routes/post_register.rb
+++ routes/post_register.rb
@@ -3,7 +3,18 @@ module Post
 
     def self.registered(app)
       app.post '/register' do
+        user_data = { first_name: params['firstname'], last_name: params['lastname'],
+                      username: params['username'], password: params['password'],
+                      email: params['email'], premium: false }
+
         
+        user = User.create(user_data)
+        settings_data = {zoom_level: 4, language: "English", user: user}
+        Setting.create(settings_data)
+
+
+        session[:user_id] = user.id
+        redirect '/home'
       end
     end
 

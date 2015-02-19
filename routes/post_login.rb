ae9427dcde19c732bc9066648ae6ef60ef9ebee7
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-02-19 10:29:09 +0100

    Redirects to home on authenticate Fixes #41

--- routes/post_login.rb
+++ routes/post_login.rb
@@ -3,7 +3,10 @@ module Post
 
     def self.registered(app)
       app.post '/login' do 
-
+        if User.authenticate(params['username'], params['password'])
+          session[:user_id] = User.first(:username => 'username')
+          redirect '/home'
+        end
       end
     end
   end 

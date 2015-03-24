2906329d213f0f52783c13711f9e9af7d75ad93c
itgsod-oscar-johansson <oscar.johansson3@Administrators-MacBook-Pro-60.local>
2015-03-24 13:06:23 +0100

    fixed bugg in post settings route #58

--- routes/post_settings.rb
+++ routes/post_settings.rb
@@ -6,13 +6,15 @@ module Post
         user_id = session[:user_id]
         current_password, new_password, confirm_password = params['current-password'], params['new-password'], params['confirm-password']
         current_email, new_email = params['current-email'], params['new-email']
-        color, zoom_level = params['color'], params['zoom-level']
+        color_id, zoom_level = params['color'], params['zoom-level']
 
+        puts "################"
+        puts params
 
-        User.change_email(user_id, current_email, new_email, current_password)
+        User.change_email(user_id, current_password, current_email, new_email)
         User.change_password(user_id, current_password, new_password, confirm_password)
         User.set_zoom_level(user_id, zoom_level)
-        User.set_color(user_id, color)
+        User.set_color(user_id, color_id)
       end
     end
 

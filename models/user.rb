e6d408866edca8ee8a65c1d258b5eb67c1af6f75
itgsod-oscar-johansson <oscar.johansson3@Administrators-MacBook-Pro-60.local>
2015-03-24 10:45:50 +0100

    added functions in user.rb

--- models/user.rb
+++ models/user.rb
@@ -25,5 +25,39 @@ class User
     user = User.first(:username => username)
     user && user.password == password
   end
+
+  def self.change_email(user_id, password, new_email, current_email)
+    puts user_id
+    user = User.get(user_id.to_i)
+    if user.password == password
+      if user.email == current_email
+        user.email = new_email
+      end
+    end
+  end
+
+  def self.change_password(user_id, current_password, new_password, confirm_password)
+    user = User.get(user_id.to_i)
+    if user.password == current_password && confirm_password == new_password
+      user.password = new_password
+    end
+  end
+
+  def self.set_zoom_level(user_id, zoom_level)
+    zoom_level = zoom_level.to_i
+    if zoom_level > 0 && zoom_level < 5
+      user = User.get(user_id.to_i)
+      user.setting.zoom_level = zoom_level
+    end
+  end
+
+  def self.set_color(user_id, color)
+    if Color.first(color_name: color)
+      user= User.get(user_id.to_i)
+      user.setting.color = color
+    end
+  end
+
+
 end
 

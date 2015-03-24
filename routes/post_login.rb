687ffe224f0b60421831e0a4c5f155cf36835031
itgsod-oscar-johansson <oscar.johansson3@Administrators-MacBook-Pro-60.local>
2015-03-24 13:07:36 +0100

    Daniel, it's me pontus. I did STUFF -fredrik
    Oscar said it was ok for me to do this commit

--- routes/post_login.rb
+++ routes/post_login.rb
@@ -2,9 +2,10 @@ module Post
   module Login
 
     def self.registered(app)
-      app.post '/login' do 
-        if User.authenticate(params['username'], params['password'])
-          session[:user_id] = User.first(:username => params['username']).id
+      app.post '/login' do
+        user = User.first(username:params['username'])
+        if user.authenticate(params['password'])
+          session[:user_id] = user.id
           redirect '/home'
         end
       end

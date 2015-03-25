64c87a247ede8e795d99868b0cb9f38d0548e370
itgsod-pontus-egebergjacob <pontus.egebergjacob@Pex-MacBook.local>
2015-03-25 13:41:16 +0100

    made som design changes to find tour

--- routes/get_findtour.rb
+++ routes/get_findtour.rb
@@ -3,10 +3,10 @@ module Get
 
     def self.registered(app)
         app.get '/findtour/:sidenumber' do |sidenumber|
-        @sidenumber = sidenumber
+        @sidenumber = sidenumber.to_i
         @user = User.get(1)
         #@user = User.get(session[:user_id])
-        @tours = Tour.all
+        @tours = Tour.all[12 * @sidenumber...12 + (@sidenumber * 12)]
         slim :findtour
       end
     end

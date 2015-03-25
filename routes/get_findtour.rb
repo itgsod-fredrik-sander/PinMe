be9ccf93657cab2ce5f973adc84b5da51ac8189a
itgsod-pontus-egebergjacob <pontus.egebergjacob@Pex-MacBook.local>
2015-03-25 11:54:30 +0100

    made the base for find tour fixes #65

--- routes/get_findtour.rb
+++ routes/get_findtour.rb
@@ -2,7 +2,8 @@ module Get
   module Findtour
 
     def self.registered(app)
-        app.get '/findtour' do
+        app.get '/findtour/:sidenumber' do |sidenumber|
+        @sidenumber = sidenumber
         @user = User.get(1)
         #@user = User.get(session[:user_id])
         @tours = Tour.all

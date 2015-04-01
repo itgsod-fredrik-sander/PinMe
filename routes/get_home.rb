5339e5897b16e63a1a40f829715713f6884c3b11
itgsod-pontus-egebergjacob <pontus.egebergjacob@Pex-MacBook.local>
2015-04-01 13:14:20 +0200

    made it redirect if you were not logged in fixes #82

--- routes/get_home.rb
+++ routes/get_home.rb
@@ -2,9 +2,13 @@ module Get
   module Home
 
     def self.registered(app)
-      app.get '/home' do 
-        @user = User.get(1)
-        #@user = session[:user]
+      app.get '/home' do
+        unless session[:user]
+          flash[:error] = 'You must sign in'
+          redirect '/login'
+        end
+        #@user = User.get(1)
+        @user = session[:user]
         @colors = Color.all
         slim :home
       end

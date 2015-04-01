7fe683839b71dbd157133260717f2c40f74e96ee
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-01 12:39:01 +0200

    Default user for home

--- routes/get_home.rb
+++ routes/get_home.rb
@@ -3,7 +3,8 @@ module Get
 
     def self.registered(app)
       app.get '/home' do 
-        @user = session[:user]
+        @user = User.get(1)
+        #@user = session[:user]
         @colors = Color.all
         slim :home
       end

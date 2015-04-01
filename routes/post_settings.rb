21e695a9c51f5c67c1a461fa10085c1eb9bf8394
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-01 14:20:54 +0200

    Moved functionality to user

--- routes/post_settings.rb
+++ routes/post_settings.rb
@@ -3,15 +3,8 @@ module Post
 
     def self.registered(app)
       app.post '/settings' do
-        user_id = session[:user_id]
-        current_password, new_password, confirm_password = params['current-password'], params['new-password'], params['confirm-password']
-        current_email, new_email = params['current-email'], params['new-email']
-        color_id, zoom_level = params['color'], params['zoom-level']
-
-        User.change_email(user_id, current_password, current_email, new_email)
-        User.change_password(user_id, current_password, new_password, confirm_password)
-        User.set_zoom_level(user_id, zoom_level)
-        User.set_color(user_id, color_id)
+        user = User.get(session[:user_id])
+        user.update_settings(params)
       end
     end
 

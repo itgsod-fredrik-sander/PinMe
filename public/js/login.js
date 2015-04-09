cdca74b820d3776337c839f551508cd427441c65
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-09 17:22:12 +0200

    Revamp of login page Fixes #52 #84

--- public/js/login.js
+++ public/js/login.js
@@ -1,26 +1,3 @@
-$(document).ready(function() {
-  var registerRectVisible = false;
-  $("#register-rectangle").hide();
-  $("#create-rectangle").hide();
-
-  $("#register-button").click(function() {
-    if (!registerRectVisible)
-    {
-      $("#register-rectangle").fadeIn();
-      $("#create-rectangle").fadeIn();
-      $("#register-button").html("Hide");
-      registerRectVisible = true;
-    } 
-    else 
-    {
-      $("#register-rectangle").fadeOut();
-      $("#create-rectangle").fadeOut();
-      $("#register-button").html("Register");
-      registerRectVisible = false;
-    }
-  })
-
-  $("#create-account-button").click(function() {
-    $("#account-form").submit();
-  })
+$('#register-button').click(function() {
+  toggleVisibility($('#register-rectangle'));
 });

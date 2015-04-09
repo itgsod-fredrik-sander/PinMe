2c525cc5c274da3242a1c5f2320acf1c81afc9fe
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-09 15:12:02 +0200

    Register cleanup #81

--- app.rb
+++ app.rb
@@ -72,15 +72,11 @@ class App < Sinatra::Base
   end
 
   post '/register' do
-    user_data = { first_name: params['firstname'], last_name: params['lastname'], username: params['username'], 
-                  password: params['password'], email: params['email'], premium: false}
-
-    user = User.create(user_data)
+    user = User.create(first_name: params['firstname'], last_name: params['lastname'], username: params['username'], password: params['password'], email: params['email'], premium: false)
 
     if user.errors.empty?
-      settings_data = {zoom_level: 4, user: user, color_id: 1}
-      Setting.create(settings_data)
-      session[:user_id] = User.first(username: user.username).id
+      Setting.create(zoom_level: 4, user: user, color_id: 1)
+      session[:user_id] = user.id
       redirect '/home'
     else
       flash[:error] = user.errors.values.flatten

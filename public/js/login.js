fed062112abb84182d44cb34ddbddcd6fe9ebd95
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-10 13:24:55 +0200

    Scrolls to registration div on btn click Fixes #91

--- public/js/login.js
+++ public/js/login.js
@@ -1,3 +1,11 @@
+function scrollToElement(element, time) {
+  if (typeof(time) === 'undefined') {time = 1000}
+
+  var pos = $(element).offset().top;
+  $('body, html').animate({scrollTop: pos}, time);
+}
+
 $('#register-button').click(function() {
   toggleVisibility($('#register-rectangle'));
+  scrollToElement($('#register-rectangle'), 300);
 });

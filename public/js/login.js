9f3d61c4959ce6f41d05a1d9b31d4e5908825e10
Freddan962 <fredrik.sander@Fredriks-MacBook-Pro.local>
2015-04-10 17:24:41 +0200

    Parallax scrolling for image-header Fixes #93

--- public/js/login.js
+++ public/js/login.js
@@ -9,3 +9,15 @@ $('#register-button').click(function() {
   toggleVisibility($('#register-rectangle'));
   scrollToElement($('#register-rectangle'), 300);
 });
+
+// 1 is the ratio of which the image should follow (1 = 100 of movement)
+// Reduce the amount by 70px to make sure there is not a white space between navbar and image
+
+var yPos, image;
+function parallax() {
+  yPos = window.pageYOffset;
+  image = document.getElementById('header-image');
+  image.style.top = yPos * 1 - 70 + 'px';
+}
+
+window.addEventListener('scroll', parallax);
